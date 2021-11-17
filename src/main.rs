8cb1c0b426799296f184cbb032e53b8d15e2a219
Philip Luyckx <philip.luyckx@gmail.com>
2021-11-17 21:40:32 +0100

    Cleanup code
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 use hangman_lib::Game;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     println!("Welcome to the hangman game!");
+
+    let word = ask_search_word();
+    let mut game = Game::new(&word, 5);
+
+    while (game.tries_left() > 0) && (!game.word_found()) {
+        let guesses = ask_letters();
+
+        handle_guesses(&mut game, guesses);
+
+        print_stats(&game);
+    }
+
+    if game.word_found() {
+        println!("Congratulations, you found the word '{}'", game.word());
+    } else {
+        println!("Oooohh, you didn't find the word '{}'", game.word());
+    }
+}
+
+fn ask_search_word() -> String {
     println!("Enter a word to search: ");
 
     let mut buffer = String::new();
@@ -12,43 +32,37 @@ fn main() {
         panic!("{}", err.to_string());
     }
 
-    buffer = buffer.to_lowercase();
-    buffer = buffer.trim().to_string();
-    let mut game = Game::new(&buffer, 5);
+    buffer.to_lowercase().trim().to_string()
+}
 
-    while (game.tries_left() > 0) && (!game.word_found()) {
-        println!("Enter one or more letters: ");
-        buffer.clear();
-        let result = io::stdin().read_line(&mut buffer);
+fn ask_letters() -> Vec<char> {
+    let mut buffer = String::new();
+    print!("Enter one or more letters: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
 
-        if let Err(err) = result {
-            panic!("{}", err.to_string());
+    buffer.to_lowercase().trim().chars().into_iter().collect()
+}
+
+fn print_stats(game: &Game) {
+    let guessed_word: String = game.guessed_word().into_iter().collect();
+
+    println!();
+    println!("Tries left: {}", game.tries_left());
+    println!("{}", guessed_word);
+    println!();
+}
+
+fn handle_guesses(game: &mut Game, guesses: Vec<char>) {
+    for ch in guesses {
+        if game.tries_left() == 0 {
+            break;
         }
 
-        buffer = buffer.to_lowercase();
-
-        for ch in buffer.trim().chars() {
-            if game.tries_left() == 0 {
-                break;
-            }
-
-            match game.guess(ch) {
-                hangman_lib::GuessResult::Wrong => println!("{} is not used in the word", ch),
-                hangman_lib::GuessResult::Correct => println!("{} is found!", ch),
-                hangman_lib::GuessResult::Repeat => println!("{} is already used as guess.", ch),
-            }
+        match game.guess(ch) {
+            hangman_lib::GuessResult::Wrong => println!("{} is not used in the word", ch),
+            hangman_lib::GuessResult::Correct => println!("{} is found!", ch),
+            hangman_lib::GuessResult::Repeat => println!("{} is already used as guess.", ch),
         }
-
-        let guessed_word: String = game.guessed_word().into_iter().collect();
-        println!();
-        println!("Tries left: {}", game.tries_left());
-        println!("{}", guessed_word);
-        println!();
-    }
-
-    if game.word_found() {
-        println!("Congratulations, you found the word '{}'", game.word());
-    } else {
-        println!("Oooohh, you didn't find the word '{}'", game.word());
     }
 }
